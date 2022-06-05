07d3b79e58c8c0598c1fe4a7cb6bd2d89627df70
senthu macbook <senthuranselvanathanbusiness@gmail.com>
2022-06-06 00:46:58 +0200

    Implement settings page

---
+++ pull_flutter/lib/views/settings.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+/// Setting page of this app, containing a link for our github and website
+class SettingsPage extends ConsumerStatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<SettingsPage> createState() => _PullSettingsPageState();
+}
+
+class _PullSettingsPageState extends ConsumerState<SettingsPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Settings'),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.deepPurple),
+              onPressed: () => Navigator.pop(context, false),
+            )),
+        body: Material(
+          child: ListView(children: [
+            for (var i = 0; i < 10; i++)
+              ListTile(
+                title: Text("Setting $i"),
+                textColor: Colors.purpleAccent,
+              ),
+            ListTile(
+              title: Text('Visit us',
+                  style: GoogleFonts.nunito(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purpleAccent,
+                      fontSize: 12)),
+              onTap: () {
+                launchUrl(Uri.parse('https://pull-dating.com'));
+              },
+            ),
+            ListTile(
+              title: Text('Discover our Code on Github',
+                  style: GoogleFonts.nunito(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purpleAccent,
+                      fontSize: 12)),
+              onTap: () {
+                launchUrl(Uri.parse(
+                    'https://github.com/PullDating/FlutterApplication'));
+              },
+            )
+          ]),
+        ));
+  }
+}
