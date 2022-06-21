7ffc25eb0c86bdbb120e2e69b02f8812ae3a846f
William Dormer <williamdormer@gmail.com>
2022-06-21 19:43:08 -0400

    minor changes to profile creation page template

--- pull_flutter/lib/model/profile_creation_base.dart
+++ pull_flutter/lib/model/profile_creation_base.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pull_common/src/model/provider/create_account.dart';
 
 class ProfileCreationTemplate extends ConsumerStatefulWidget {
   const ProfileCreationTemplate({Key? key}) : super(key: key);
 
   @override
-  ConsumerState createState() => _ProfileCreationTemplateState();
+  ConsumerState <ProfileCreationTemplate> createState() => _ProfileCreationTemplateState();
 }
 
-class _ProfileCreationTemplateState extends ConsumerState {
+class _ProfileCreationTemplateState extends ConsumerState<ProfileCreationTemplate> {
+  @override
+  void initState() {
+    super.initState();
+    final profile = ref.read(accountCreationProfile);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final profile = ref.watch(accountCreationProfile);
+    return Test();
+  }
+}
+
+class Test extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("hello");
   }
 }
