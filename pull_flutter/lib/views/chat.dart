843c5af39ee2700c0dc84d4c31246fb3c758c894
William Dormer <williamdormer@gmail.com>
2022-06-27 10:58:54 -0400

    Chat ui screen bare bones is working.

--- pull_flutter/lib/views/chat.dart
+++ pull_flutter/lib/views/chat.dart
@@ -1,4 +1,15 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+
+String randomString(){
+  final random = Random.secure();
+  final values = List<int>.generate(16,(i) => random.nextInt(255));
+  return base64UrlEncode(values);
+}
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key, required this.uuid}) : super(key: key);
@@ -10,14 +21,51 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final List<types.Message> _messages = []; //list of messages
+  var _user;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _user = types.User(id: widget.uuid);
+  }
+
+
+  void _addMessages(types.Message message){
+    setState(() {
+      _messages.insert(0,message);
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message){
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: randomString(),
+      text: message.text,
+    );
+
+    _addMessages(textMessage);
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: SafeArea(
-        child: Container(
-          child: Text("${widget.uuid}"),
+        child: Scaffold(
+          body: Chat(
+            messages: _messages,
+            onSendPressed: _handleSendPressed,
+            user: _user,
+          ),
         ),
       ),
     );
+
+
+
   }
 }
