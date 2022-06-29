061dae70c7c9de362ba031d70adbc896d05ab2ac
William Dormer <williamdormer@gmail.com>
2022-06-29 10:20:19 -0400

    added firebase_messaging
    Starting the setup of the firebase messaging for push notifications.

--- pull_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pull_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,15 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_messaging
 import isar_flutter_libs
 import path_provider_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
